ec4f1eaebaf2afd490125e4665bd0197e08882fd
WhyPhy <yogamuslim21@gmail.com>
2016-10-15 17:19:12 +0700

    Pembuatan Layout

---
+++ app/src/main/java/com/example/smktelkomspmalang/tugas001/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.smktelkomspmalang.tugas001;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
